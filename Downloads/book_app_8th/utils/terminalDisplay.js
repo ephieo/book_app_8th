e7c78bf70a79d855317047bccef093ecb2d9a62f
ephie oyedoh <ephieo@rocketmail.com>
2021-06-13 22:22:24 +0100

    reorganising utils folder to make the functions easier to read.

--- Downloads/book_app_8th/utils/terminalDisplay.js
+++ Downloads/book_app_8th/utils/terminalDisplay.js
@@ -37,10 +37,5 @@ function returnReadingList(username) {
     //console.log(shelf, name);
   });
 }
-//findUsername(shelf, username);
-// function findUsername(shelf, username) {
-//   let name = Object.keys(shelf.users).filter((e) => e === `${username}`);
-//   return name;
-// }
 
 export { returnBookSelection, returnReadingList };
