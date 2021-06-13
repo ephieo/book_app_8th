1f627965de930ab7bfaba9abebe234e2bd5d1526
ephie oyedoh <ephieo@rocketmail.com>
2021-06-13 22:11:58 +0100

    extracting fetch request to it's own file for further modularisation

--- Downloads/book_app_8th/index.js
+++ Downloads/book_app_8th/index.js
@@ -1,15 +1,12 @@
 import promptSync from 'prompt-sync';
-import fetch from 'node-fetch';
 import dotenv from 'dotenv';
-import { returnBookSelection, findUsername } from './utils/utils.js';
+import fetchFromApi from './utils/fetch.js';
 
 dotenv.config();
 
 let key = process.env.API_KEY;
 
 const prompt = promptSync();
-
-let response;
 
 let title = prompt('What is the name of the book ? ');
 //console.log(title);
@@ -19,26 +16,4 @@ let author = prompt("What is the author's name ? ");
 
 let username = prompt('What is your username ? ');
 
-fetch(
-  `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&${key}&maxResults=5`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    response = data.items.map(
-      (e) => `{
-        title:${!e.volumeInfo.title ? 'No title found' : e.volumeInfo.title},
-        author:${
-          !e.volumeInfo.authors ? 'No Author found' : e.volumeInfo.authors
-        },
-        company:${
-          !e.volumeInfo.publisher
-            ? 'Publisher not found'
-            : e.volumeInfo.publisher
-        },
-   }`
-    );
-
-    console.log(response.map((e) => `Book ID :${response.indexOf(e)} ${e}`));
-    returnBookSelection(username, response);
-  })
-  .catch((err) => console.log(err));
+fetchFromApi(title, author, username, key);
---
+++ Downloads/book_app_8th/utils/fetch.js
@@ -0,0 +1,31 @@
+import { returnBookSelection, findUsername } from './utils.js';
+import fetch from 'node-fetch';
+
+export default function fetchFromApi(title, author, username, key) {
+  let response;
+  fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&${key}&maxResults=5`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      response = data.items.map(
+        (e) => `{
+            title:${
+              !e.volumeInfo.title ? 'No title found' : e.volumeInfo.title
+            },
+            author:${
+              !e.volumeInfo.authors ? 'No Author found' : e.volumeInfo.authors
+            },
+            company:${
+              !e.volumeInfo.publisher
+                ? 'Publisher not found'
+                : e.volumeInfo.publisher
+            },
+       }`
+      );
+
+      console.log(response.map((e) => `Book ID :${response.indexOf(e)} ${e}`));
+      returnBookSelection(username, response);
+    })
+    .catch((err) => console.log(err));
+}
