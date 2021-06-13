1170f82b75e1f90cde2c5d4b52c48141c7ea2e95
ephie oyedoh <ephieo@rocketmail.com>
2021-06-13 21:32:37 +0100

    writing util tests

--- Downloads/book_app_8th/utils.test.js
+++ Downloads/book_app_8th/utils.test.js
@@ -1,5 +1,30 @@
-import { sum } from './utils/utils.js';
+import {
+  returnBookSelection,
+  returnReadingList,
+  sum,
+  findUsername,
+} from './utils/utils.js';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test(' findUsername function should return an array containing a username.', () => {
+  expect(findUsername(testData.testObj, testData.username)).toEqual(['moon']);
+});
+
+test(' function should display the returned fetch data with the bookshelf format', () => {
+  expect(
+    returnBookSelection(testData.username, testData.testObj)
+  ).toHaveBeenCalled();
+});
+
+let testData = {
+  testObj: {
+    users: {
+      moon: 'ReadingList:[{\n        title:New Moon, Writing,\n        author:New Moon Books Girls Editorial Board,\n        company:Crown Pub,\n   }}]',
+    },
+  },
+  username: 'moon',
+  //testfunc: jest.fn(),
+};
