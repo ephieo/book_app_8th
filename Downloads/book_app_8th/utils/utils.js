9f47347f4e8b7f2ff1c174123f7299d7c674722b
ephie oyedoh <ephieo@rocketmail.com>
2021-06-15 10:52:33 +0100

    adding comments explaining functionality to the utils file

--- Downloads/book_app_8th/utils/utils.js
+++ Downloads/book_app_8th/utils/utils.js
@@ -1,5 +1,7 @@
-function findUsername(shelf, username) {
-  let name = Object.keys(shelf.users).filter((e) => e === `${username}`);
+/* This function takes in response takes formatted data from the so called "bookshelf" and the username of the user
+to check if the users username exists  */
+function findUsername(bookshelf, username) {
+  let name = Object.keys(bookshelf.users).filter((e) => e === `${username}`);
   return name;
 }
 
