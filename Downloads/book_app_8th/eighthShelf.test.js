bb7ff4d98e785c4c54db99f1e097789f7a85309f
ephie oyedoh <ephieo@rocketmail.com>
2021-06-16 22:38:03 +0100

    working on tests

--- Downloads/book_app_8th/eighthShelf.test.js
+++ Downloads/book_app_8th/eighthShelf.test.js
@@ -1,13 +1,28 @@
-import { findUsername, colourText } from './utils/utils.js';
-//import { fetchReadingList } from './utils/terminalDisplay.js';
+import { findUsername, colourText, errorMessage } from './utils/utils.js';
+import { jest } from '@jest/globals';
 
 test(' findUsername function should return an array containing a username.', () => {
   expect(findUsername(testData.testObj, testData.username)).toEqual(['moon']);
 });
 
-test(' colourText takes two strings and should return one coloured string.', () => {
-  expect(colourText(testData.string, testData.colour)).toEqual(['green']);
+// test(' colourText takes two strings and should return one coloured string.', () => {
+//   expect(colourText(testData.string, testData.colour)).toEqual(['green']);
+// });
+
+describe('Tests my console.log', () => {
+  console.log = jest.fn();
+  it('should console a message', () => {
+    console.log('My test is working with console.log');
+
+    expect(console.log).toHaveBeenCalledWith(
+      'My test is working with console.log'
+    );
+  });
 });
+
+// test('testing mock implementation', () => {
+//   const mock = jest.fn.mockImplementation(errorMessage());
+// });
 
 // test(' fetchReadingList() function should return the users reading list.', () => {
 //   expect(fetchReadingList(testData.username)).toContain(`readingList: [
